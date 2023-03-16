569a3aa0de9a7fbddb38a1ce2cfc96944d1b38b0
22125546 <dillonl2003@outlook.com>
2023-03-16 15:14:58 +0200

    searches for specified user and allows admin to update email

--- backend/TATP-36.py
+++ backend/TATP-36.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql+mysql connector://admin:password@localhost/mydb')
+engine = create_engine('mysql+mysqlconnector://admin:password@localhost/mydb')
 
 Session = sessionmaker(bind=engine)
 
@@ -14,37 +14,41 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    email = Column(String(50))
+    given_name = Column(String(50))
+    family_name = Column(String(50))
+    name = Column(String(100))
 
 
 # create session
 session = Session()
 
-
-class UpdateUserEmail:
+class UpdateUser:
     def __init__(self):
         self.user_id = input("Enter user id: ")
-        self.email = input("Enter new email: ")
+        self.given_name = input("Enter given name: ")
+        self.family_name = input("Enter family name: ")
+        self.name = input("Enter name: ")
 
-    def update_email(self):
+    def update_user(self):
         # retrieve user by id
-        user = session.query(User).filter_by(id=self.user_id)
+        user = session.query(User).filter_by(id=self.user_id).first()
 
         if not user:
             print("User not found.")
             return
 
-        # update user email
-        user.email = self.email
+        # update user attributes
+        user.given_name = self.given_name
+        user.family_name = self.family_name
+        user.name = self.name
         session.commit()
-        print("User email updated successfully.")
+        print("User updated successfully.")
 
     def __del__(self):
         # close session
         session.close()
 
 
-# create instance of UpdateUserEmail and call update_email() method
-update_user_email = UpdateUserEmail()
-update_user_email.update_email()
+# create instance of UpdateUser and call update_user() method
+update_user = UpdateUser()
+update_user.update_user()
